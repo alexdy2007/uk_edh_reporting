195ffdf93d029d755db935ceca713ad6468d0426
Alex Young <alex.young@databricks.com>
2025-03-24 15:21:46 +0000

    Update EDM Tags.py

--- EDM Tags.py
+++ EDM Tags.py
@@ -1,12 +1,12 @@
 # Databricks notebook source
 from pyspark.sql import functions as SF 
 
-df = (spark.read.table('water_demos.united_utilities.example_uu_devices_edm')
+df = (spark.read.table('water_demos.schema_1.example_uu_devices_edm')
 .withColumn('Start',SF.to_timestamp(SF.col('Start'),'d/M/y H:m:s'))
 .withColumn('End',SF.to_timestamp(SF.col('End'),'d/M/y H:m:s'))
 )
 
-df.write.mode('overwrite').option("overwriteSchema", "true").saveAsTable('water_demos.united_utilities.example_uu_devices_edm')
+df.write.mode('overwrite').option("overwriteSchema", "true").saveAsTable('water_demos.schema_1.example_uu_devices_edm')
 
 # COMMAND ----------
 
@@ -16,41 +16,41 @@ display(df)
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE or REPLACE VIEW water_demos.united_utilities.devices_all_intervals AS
+# MAGIC CREATE or REPLACE VIEW water_demos.schema_1.devices_all_intervals AS
 # MAGIC SELECT DISTINCT
 # MAGIC site_id,
 # MAGIC `Start` AS time_interval
 # MAGIC FROM
-# MAGIC water_demos.united_utilities.example_uu_devices_edm
+# MAGIC water_demos.schema_1.example_uu_devices_edm
 # MAGIC WHERE site_id=1
 # MAGIC UNION
 # MAGIC SELECT DISTINCT
 # MAGIC site_id,
 # MAGIC `End` AS time_interval
 # MAGIC FROM
-# MAGIC water_demos.united_utilities.example_uu_devices_edm
+# MAGIC water_demos.schema_1.example_uu_devices_edm
 # MAGIC WHERE site_id=1
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * FROM water_demos.united_utilities.devices_all_intervals
+# MAGIC SELECT * FROM water_demos.schema_1.devices_all_intervals
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE OR REPLACE VIEW water_demos.united_utilities.device_all_time_windows AS
+# MAGIC CREATE OR REPLACE VIEW water_demos.schema_1.device_all_time_windows AS
 # MAGIC SELECT
 # MAGIC site_id,
 # MAGIC time_interval AS start_time,
 # MAGIC LEAD(time_interval, 1) OVER (PARTITION BY site_id ORDER BY time_interval) AS end_time
 # MAGIC FROM
-# MAGIC water_demos.united_utilities.devices_all_intervals
+# MAGIC water_demos.schema_1.devices_all_intervals
 
 # COMMAND ----------
 
 # MAGIC %sql 
-# MAGIC SELECT * FROM water_demos.united_utilities.device_all_time_windows
+# MAGIC SELECT * FROM water_demos.schema_1.device_all_time_windows
 
 # COMMAND ----------
 
@@ -61,8 +61,8 @@ display(df)
 # MAGIC w.end_time,
 # MAGIC SUM(CASE WHEN l.site_id IS NOT NULL THEN 1 ELSE 0 END) AS active_sensors
 # MAGIC FROM
-# MAGIC water_demos.united_utilities.device_all_time_windows w
-# MAGIC LEFT JOIN water_demos.united_utilities.example_uu_devices_edm l
+# MAGIC water_demos.schema_1.device_all_time_windows w
+# MAGIC LEFT JOIN water_demos.schema_1.example_uu_devices_edm l
 # MAGIC ON w.site_id = l.site_id
 # MAGIC AND w.start_time < l.end
 # MAGIC AND w.end_time >= l.start
